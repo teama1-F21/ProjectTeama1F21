fe3a3ff4ed14b61b61693dbba523f8d34f1beee1
Neha-Patil22 <nbp30352@creighton.edu>
2021-11-10 15:10:24 -0600

    v1
    Login needs a loop and an if/else statement inside of it.

--- 375ProjectTeama1.appstudio/forms/login/login.js
+++ 375ProjectTeama1.appstudio/forms/login/login.js
@@ -1,1 +1,35 @@
 //login
+
+let query = ""
+
+let req = ""
+
+let results = []
+
+let pw = "Fluffy.2013"
+
+  let netID = "nbp30352"
+
+let teamID = "375groupa1"
+
+
+btnLogin.onclick = function() {
+    // use select query
+    query = "Select username,passcode FROM clients;"
+    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + teamID + "&query=" + query)
+
+    let username = username.value
+    let password = password.value
+    // use a loop here
+    while () {
+    /*if ((username == ' ') && (password == '')) {
+      ChangeForm(homePage)
+    } else {
+      lblMessage.textContent = "Try Again"
+    }*/
+}
+
+
+    Button1.onclick = function() {
+      ChangeForm(newAccount)
+    }
