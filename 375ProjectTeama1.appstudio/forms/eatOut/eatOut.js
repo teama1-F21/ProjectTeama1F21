89abddeafe0f7c6c0d97a82fb9382aca4d0e9757
annawhitehead <annawhitehead00@gmail.com>
2021-11-15 01:12:07 -0600

    correct dropdown dineIn

--- 375ProjectTeama1.appstudio/forms/eatOut/eatOut.js
+++ 375ProjectTeama1.appstudio/forms/eatOut/eatOut.js
@@ -1,21 +1,21 @@
 let query= ""
 let req=""
-let results=[]
 let pw="LucyiSgetting0ld69420"
 let netID="ajw62691"
 let teamID="375groupa1"
 
 req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + teamID + "&query=" + query)
 
-let FoodType=[]
+
 eatOut.onshow=function(){
 optionsDrp.clear()
+let FoodType=["hearty","dessert","fish","pizza"]
     for (i = 0; i < FoodType.length; i++)
         optionsDrp.addItem(FoodType[i])
 }
 
 
-submitEatOut.onclick=function(s){
+optionsDrp.onclick=function(s){
   if (typeof(s) == "object"){  // means the control was clicked
     return                     // but no selection made yet so do nothing
   } else {                     // a selection made
@@ -23,4 +23,7 @@ submitEatOut.onclick=function(s){
     console.log(`The user chose ${s} and .selection is ${optionsDrp.selection}.`)
   }
   }
+submitEatOut.onclick=function(){
+
+}
 
