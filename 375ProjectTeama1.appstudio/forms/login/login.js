b7fc02c4d3909c0bd4a1439bd3265bd1c872c53f
Neha-Patil22 <nbp30352@creighton.edu>
2021-11-20 00:49:52 -0600

    v3
    done

--- 375ProjectTeama1.appstudio/forms/login/login.js
+++ 375ProjectTeama1.appstudio/forms/login/login.js
@@ -1,4 +1,6 @@
 //login
+
+/*
 let query = ""
 
 let req = ""
@@ -13,30 +15,24 @@ let teamID = "375groupa1"
 
 
 btnLogin.onclick = function() {
-    // use select query
-    let username1 = username.value
-    
-    let password1 = password.value
-    // need remove space b4 and after the value
-    query = "Select username,passcode FROM clients WHERE username = ' "+username1+" ' and passcode =' "+password1+" ' "
-    alert(query)
-    
-    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + teamID + "&query=" + query)
+  let username1 = username.value
+  let password1 = password.value
+  query = "Select username,passcode FROM clients WHERE username = '" + username1 + "' and passcode ='" + password1 + "' "
+
+  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + teamID + "&query=" + query)
+  if (req.status == 200) {
     results = JSON.parse(req.responseText)
-    console.log(results)
 
-    // use a loop here
-
-    if (query == true) {
-      ChangeForm(homePage)
-    } else {
+    if (results.length == 0) {
       lblMessage.value = "Try Again"
+    } else {
+      ChangeForm(homePage)
     }
-    
-    
+  }
 }
 
 
-btnNewAccount.onclick=function(){
+btnNewAccount.onclick = function() {
   ChangeForm(newAccount)
 }
+*/
